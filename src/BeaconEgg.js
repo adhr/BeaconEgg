0140f8d96aa0056e77285b645fa2bf0c71bb40f1
itani-hiroki <itanihiroki@gmail.com>
2013-12-14 17:57:12 +0900

    modified params

--- src/BeaconEgg.js
+++ src/BeaconEgg.js
@@ -5,10 +5,11 @@
     /**
      * iBeacon 
      * @constructor
-     * @extends beaconegg.EventDispatcher
+     * @extends adhr.EventDispatcher
      */
     function BeaconEgg() {
         win.beaconegg = this;
+        this.regions  = [];
     }
 
     BeaconEgg.prototype = new ns.EventDispatcher;
@@ -16,33 +17,56 @@
     /**
      * @const {string}
      */
-    BeaconEgg.EVENT_ENTER_REGION = 'beaconegg.event.enter.region';
+    BeaconEgg.EVENT_ENTER_REGION = 'adhr.beaconegg.event.enter.region';
 
     /**
      * @const {string}
      */
-    BeaconEgg.EVENT_EXIT_REGION = 'beaconegg.event.exit.region';
+    BeaconEgg.EVENT_EXIT_REGION = 'adhr.beaconegg.event.exit.region';
 
     /**
      * @const {string}
      */
-    BeaconEgg.EVENT_RENGE = 'beaconegg.event.renge';
+    BeaconEgg.EVENT_RENGE = 'adhr.beaconegg.event.renge';
+
+    /**
+     * @param {string} uuid region uuid
+     * @return {integer}
+     */
+    BeaconEgg.prototype.findRegionIndexByUUID = function (uuid) {
+        var _regions = this.regions.concat([]);
+        for (var i = 0; i < _regions.length; ++i) {
+            if (_regions[i].uuid === uuid) {
+                return i;
+            }
+        }
+    }
+
+    /**
+     * @param {string} uuid region uuid
+     * @return {adhr.Region}
+     */
+    BeaconEgg.prototype.findRegionByUUID = function (uuid) {
+        return this.regions[this.findRegionIndexByUUID(uuid)];
+    }
 
     /**
      * redirect to start monitoring
-     * @param  {beaconegg.Region} region Region object
+     * @param  {adhr.Region} region Region object
      * @return {void}
      */
     BeaconEgg.prototype.startMonitoring = function(region) {
+        this.regions.push(region);
         location.href = 'beaconegg://egg/start-monitoring?proximityUUID=' + region.uuid + '&id=' + region.id;
     };
 
     /**
      * redirect to stop monitoring
-     * @param  {beaconegg.Region} region Region object
+     * @param  {adhr.Region} region Region object
      * @return {void}
      */
     BeaconEgg.prototype.stopMonitoring = function(region) {
+        this.regions.splice(this.findRegionIndexByUUID(region.uuid), 1);
         location.href = 'beaconegg://egg/stop-monitoring?proximityUUID=' + region.uuid;
     };
 
@@ -52,7 +76,7 @@
      * @return {void}
      */
     BeaconEgg.prototype.didEnterRegion = function(data) {
-        this.dispatchEvent(BeaconEgg.EVENT_ENTER_REGION, data);
+        this.dispatchEvent(BeaconEgg.EVENT_ENTER_REGION, this.findRegionByUUID(data.proximityUUID));
     }
 
     /**
@@ -61,7 +85,7 @@
      * @return {void}
      */
     BeaconEgg.prototype.didExitRegion = function(data) {
-        this.dispatchEvent(BeaconEgg.EVENT_EXIT_REGION, data);
+        this.dispatchEvent(BeaconEgg.EVENT_EXIT_REGION, this.findRegionByUUID(data.proximityUUID));
     }
 
     /**
